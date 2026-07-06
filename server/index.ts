7d3351ed976fa641f9a7d2d5e5e373b990596d41
Shishir <shishirtech506@gmail.com>
2026-07-06 20:26:39 +0530

    backend: socket.io added

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,19 @@
 import { createServer } from "node:http";
+import { Server } from "socket.io";
 
 const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
 
 const PORT = 4000;
 
+io.on("connection", (socket) => {
+  console.log(`User connected : ${socket.id}`);
+});
+
 httpServer.listen(PORT, () => {
-    console.log(`Socket.IO server running on http://localhost:${PORT}`);
-  });
+  console.log(`Socket.IO server running on http://localhost:${PORT}`);
+});
